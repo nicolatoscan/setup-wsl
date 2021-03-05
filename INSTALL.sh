dabe485f2341d5ff50792b8696cde1e180f3723e
nicolatoscan <nicolatoscan99@outlook.it>
2021-03-05 19:31:43 +0100

    update

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,12 +4,7 @@
 apt update
 apt upgrade -y
 apt autoremove -y
-apt install build-essential mc jq nodejs curl wget git nano mc htop sudo bash zsh -y
-
-# node
-curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
-apt install nodejs -y
-npm install typescript
+apt install build-essential mc jq nodejs curl wget git nano mc htop sudo bash zsh python3 python3-pip -y
 
 # oh-my-zsh
 yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -20,11 +15,19 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # config
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
 sed -i 's/^plugins=.*/plugins=(git zsh-syntax-highlighting zsh-autosuggestions)/' ~/.zshrc
-
-# TODO: get file .p10k.zsh
-wget .p10k.zsh
-
+wget https://raw.githubusercontent.com/nicolatoscan/setup-wsl/master/.p10k.zsh -O /.p10k.zsh
 echo "# To customize prompt, run \`p10k configure\` or edit ~/.p10k.zsh." >> ~/.zshrc
 echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
+
+
+# alias
 echo "alias py=\"python3\"" >> ~/.zshrc
 echo "alias cdw=\"cd /mnt/c/Users/toscan/\"" >> ~/.zshrc
+
+# node
+curl -fsSL https://deb.nodesource.com/setup_15.x | sudo -E bash -
+apt install nodejs -y
+npm install typescript
+
+# python
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
